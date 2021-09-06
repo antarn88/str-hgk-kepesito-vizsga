e0e949fab4702b193b08c0481037a687c67f0765
Arnold <antaloczi.arnold@gmail.com>
2021-09-06 12:53:02 +0200

    Task 1. done

--- angular/src/app/components/student-form/student-form.component.ts
+++ angular/src/app/components/student-form/student-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
+import { Location } from '@angular/common'
+import { ActivatedRoute } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentHttpService } from 'src/app/service/student-http.service';
 
 @Component({
   selector: 'app-student-form',
@@ -11,14 +15,28 @@ export class StudentFormComponent implements OnInit {
   @ViewChild('form') form: NgForm;
   reactForm: FormGroup;
 
+  student: Student;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private location: Location,
+    private studentService: StudentHttpService
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const id = this.activatedRoute.snapshot.params.id;
+    const student = await this.studentService.getById(id).toPromise();
+    this.student = student;
   }
 
-  saveStudent(){
-
+  async saveStudent(form: NgForm){
+    const student = form.value;
+    try {
+      await this.studentService.update(student, this.student._id).toPromise();
+      this.location.back();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
 }
