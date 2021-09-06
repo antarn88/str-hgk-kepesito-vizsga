ea3d264021513b0f1cc2cb310109097d8a6d87fd
Arnold <antaloczi.arnold@gmail.com>
2021-09-06 13:07:27 +0200

    Task 2. done

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,28 @@
-/**
- * Exercise: make School mongoose model
- */
+const mongoose = require('mongoose');
+
+const SchoolSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String,
+    required: true
+  },
+  zipcode: {
+    type: Number,
+    required: true
+  },
+  classrooms: {
+    type: [String],
+    ref: 'Classroom',
+    required: false,
+  },
+}, {
+});
+
+module.exports = mongoose.model('School', SchoolSchema);
